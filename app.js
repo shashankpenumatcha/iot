51131678cbdd1bf9f3722ccc183e57242f01b2a0
shashankpenumatcha <shashankpenumatcha@gmail.com>
2019-12-25 01:15:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,106 @@
+
+
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var path = require('path');
+
+let connections=[];
+let state={};
+state.boards={};
+ 
+client.on('connect', function () {
+  client.subscribe('penumats/handshake/connect', function (err) {
+    if (!err) {
+      console.log('ready to shake hands');
+      client.publish('penumats/handshake/reinitiate',"hi")
+    }
+  })
+});
+ 
+client.on('message', function (topic, message) {
+  if(topic=="penumats/handshake/connect"){
+    console.log("new nmcu handshake initiated");
+    let id = JSON.parse(message.toString()).id;
+    if(id){
+      state.boards[id]=JSON.parse(message.toString());
+      console.log("board registered",JSON.stringify(state.boards));
+    }
+  }
+});
+
+
+
+app.get('/on',function(req,res){
+  if(!req.query||!req.query.b||req.query.s==undefined||req.query.s==null){
+    return res.status(400).send({error:"please check the query params"});
+  }
+  let board = req.query.b;
+  let $switch = req.query.s;
+  if(state.boards[board]&&state.boards[board].switches!=undefined&&state.boards[board].switches[$switch]!=undefined){
+    client.publish("penumats/"+board+"/switch/on",JSON.stringify({switch:parseInt($switch),state:true}));
+    res.status(200).send('on');
+  }else{
+    res.status(404).send({error:'board or switch not found'});
+  }
+
+})
+
+app.get('/off',function(req,res){
+  if(!req.query||!req.query.b||req.query.s==undefined||req.query.s==null){
+    return res.status(400).send({error:"please check the query params"});
+  }
+  let board = req.query.b;
+  let $switch = req.query.s;
+  if(state.boards[board]&&state.boards[board].switches!=undefined&&state.boards[board].switches[$switch]!=undefined){
+    client.publish("penumats/"+board+"/switch/off",JSON.stringify({switch:parseInt($switch),state:false}));
+    res.status(200).send('off');
+  }else{
+    res.status(404).send({error:'board or switch not found'});
+  }
+
+})
+
+app.get('/boards',function(req,res){
+ if(!state||!state.boards){
+   return res.status(404).send({error:"boards not found"});
+ }
+ let boards={};
+ Object.keys(state.boards).map(function(m){
+    boards[m] = {};
+    boards[m].switches=[];
+    if(state.boards[m].switches.length){
+      state.boards[m].switches.map(function(n,index){
+          let ob={};
+          ob.label=null;
+          ob.on="/on?b="+m+"&s="+index;
+          ob.off="/on?b="+m+"&s="+index;
+          ob.state = n;
+          boards[m].switches.push(ob);
+          return n;
+      });
+    }
+    
+    return m;
+ });
+ return res.status(200).send(boards);
+})
+
+app.use('/mqtt',express.static(path.join(__dirname,'node_modules/mqtt/dist')))
+app.use(express.static('public'))
+console.log(path.join(__dirname,'node_modeules/mqtt/dist'))
+
+
+
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ sketch.ino
@@ -0,0 +1,128 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "";
+const char* password = "";
+const char* mqtt_server = "test.mosquitto.org";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+String id = "nodemcu1";
+int switches[] = {BUILTIN_LED};
+StaticJsonDocument<200> doc;
+
+void setup_wifi() {
+  delay(10);
+  
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if(String(topic) == "penumats/handshake/reinitiate"){
+    Serial.print("reinitiating");
+     //client.publish("penumats/handshake/connect",("{\"id\":\""+id+"\"}").c_str());
+     char buffer[512];
+    size_t n = serializeJson(doc, buffer);
+    client.publish("penumats/handshake/connect", buffer, n);
+  }
+  
+  if(String(topic) == "penumats/"+id+"/switch/on"){
+    StaticJsonDocument<256> docx;
+    deserializeJson(docx, payload, length);
+    Serial.println();
+    int s = docx.getMember(String("switch"));
+    int pin = switches[s];
+    if(NULL!=pin){
+       Serial.println("turning on switch");
+       digitalWrite(pin, LOW);
+     }
+  }
+  
+  if(String(topic) == "penumats/"+id+"/switch/off"){
+    StaticJsonDocument<256> docx;
+    deserializeJson(docx, payload, length);
+    Serial.println();
+    int s = docx.getMember(String("switch"));
+    int pin = switches[s];
+    if(NULL!=pin){
+       Serial.println("turning off switch");
+       digitalWrite(pin, HIGH);
+    }
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(id.c_str())) {
+      Serial.println("connected");
+      client.subscribe("penumats/handshake/reinitiate");
+      client.subscribe(("penumats/"+id+"/switch/on").c_str());
+      client.subscribe(("penumats/"+id+"/switch/off").c_str());
+     // client.publish("penumats/handshake/connect", ("{\"id\":\""+id+"\"}").c_str());
+      char buffer[512];
+    size_t n = serializeJson(doc, buffer);
+    client.publish("penumats/handshake/connect", buffer, n);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  doc["id"] = "nodemcu1";
+  JsonArray switches = doc.createNestedArray("switches");
+  switches.add(true);
+  Serial.println();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  
+}
