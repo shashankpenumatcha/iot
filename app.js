184b72ec95294e2860719aae09ef13f4adfa6e84
shashankpenumatcha <shashankpenumatcha@gmai.com>
2020-01-05 00:15:33 +0530

    geT MAC

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ const bcrypt = require('bcrypt');
  var wifi = new Wifi();
 require("./wifi.js");
 
+const mac;
+fs.readFileSync('/sys/class/net/wlan0/address', 'utf8', function(err, contents) {
+  if(contents&&contents.length){
+    mac = contents.replace(':','');
+  }
+});
+console.log(mac);
 
 const deviceId='rpi1';
 var device = null; //registered device from server
@@ -30,7 +37,7 @@ var boards = []; //registered boards from server
 let connections=[];
 let state={};
 state.boards={};
-let localusers = [];
+let localusers  = [];
 
 
 
