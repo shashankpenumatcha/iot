9a503f376da940619efeff4da97a518744d71d9b
shashankpenumatcha <shashankpenumatcha@gmai.com>
2020-02-19 01:42:54 +0530

    lksjd

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ var j = schedule.scheduleJob(statsRule, function(){
     if(res&&res.length){
       res.map(m=>{
         if(m.lastOnTime){
-          handleOnForTracking(m.board,m.switch,currentTime.format())
+          handleOnForTracking(m.board,m.switch,m.lastOnTime)
           handleOffForTracking(m.board,m.switch,moment(new Date()).format())
           handleOnForTracking(m.board,m.switch)
 
